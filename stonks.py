ac04d9100dc9bdb61a034a076bf77d7926f5ea91
Aaron Smith <aaroncolesmith@gmail.com>
2021-02-01 19:31:48 -0800

    stocks

--- stonks.py
+++ stonks.py
@@ -10,7 +10,7 @@ import pytz
 st.set_page_config(layout='wide',initial_sidebar_state='collapsed')
 
 def load_data():
-    df = pd.read_csv('./data.csv')
+    df = pd.read_csv('./stonks_data.csv')
     df['occurences']=pd.to_numeric(df['occurences'])
     df['positive']=pd.to_numeric(df['positive'])
     df['negative']=pd.to_numeric(df['negative'])
